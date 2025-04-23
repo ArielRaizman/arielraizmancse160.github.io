5df1e6dc0d5b6c8e210584394e825456f8a7a3fd
Ariel Raizman <ariel@appesteem.com>
2025-04-22 22:05:24 -0700

    basic block functionality

--- asg2/Cube.js
+++ asg2/Cube.js
@@ -17,29 +17,67 @@ class Cube {
         // pass the matrix
         gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
 
-        // Front of Cube
-        drawTriangle3D(gl, [0.0,0.0,0.0,  1.0,1.0,0.0,  1.0,0.0,0.0]);
-        drawTriangle3D(gl, [0.0,0.0,0.0,  0.0,1.0,0.0,  1.0,1.0,0.0]);
+        // // Front of Cube
+        // gl.uniform4f(u_FragColor, 255, 0, 0, rgba[3]);
+        // drawTriangle3D(gl, [0.0,0.0,0.0,  1.0,1.0,0.0,  0.0,1.0,0.0]);
+        // drawTriangle3D(gl, [0.0,0.0,0.0,  1.0,0.0,0.0,  1.0,1.0,0.0]);
 
-        // Back of Cube
-        drawTriangle3D(gl, [0.0,0.0,1.0,  1.0,0.0,1.0,  1.0,1.0,1.0]);
-        drawTriangle3D(gl, [0.0,0.0,1.0,  1.0,1.0,1.0,  0.0,1.0,1.0]);
+        // // Back of Cube
+        // gl.uniform4f(u_FragColor, rgba[0]*.1, rgba[1]*.9, rgba[2]*.9, rgba[3]);
+        // gl.uniform4f(u_FragColor, 0, 255, 0, rgba[3]);
+        // drawTriangle3D(gl, [0.0,0.0,1.0,  1.0,0.0,1.0,  1.0,1.0,1.0]);
+        // drawTriangle3D(gl, [0.0,0.0,1.0,  1.0,1.0,1.0,  0.0,1.0,1.0]);
         
-        // Left of Cube
-        drawTriangle3D(gl, [0.0,0.0,0.0,  0.0,1.0,1.0,  0.0,1.0,0.0]);
-        drawTriangle3D(gl, [0.0,0.0,0.0,  0.0,0.0,1.0,  0.0,1.0,1.0]);
+        // // Left of Cube
+        // gl.uniform4f(u_FragColor, rgba[0]*.9, rgba[1]*.9, rgba[2]*.9, rgba[3]);
+        // gl.uniform4f(u_FragColor, 0, 0, 255, rgba[3]);
+        // drawTriangle3D(gl, [0.0,0.0,0.0,  0.0,1.0,1.0,  0.0,1.0,0.0]);
+        // drawTriangle3D(gl, [0.0,0.0,0.0,  0.0,0.0,1.0,  0.0,1.0,1.0]);
 
-        // Right of Cube
-        drawTriangle3D(gl, [1.0,0.0,0.0,  1.0,1.0,0.0,  1.0,1.0,1.0]);
-        drawTriangle3D(gl, [1.0,0.0,0.0,  1.0,1.0,1.0,  1.0,0.0,1.0]);
+        // // Right of Cube
+        // drawTriangle3D(gl, [1.0,0.0,0.0,  1.0,1.0,0.0,  1.0,1.0,1.0]);
+        // drawTriangle3D(gl, [1.0,0.0,0.0,  1.0,1.0,1.0,  1.0,0.0,1.0]);
 
-        // Top of Cube
-        drawTriangle3D(gl, [0.0,1.0,0.0,  1.0,1.0,1.0,  1.0,1.0,0.0]);
-        drawTriangle3D(gl, [0.0,1.0,0.0,  0.0,1.0,1.0,  1.0,1.0,1.0]);
+        // // Top of Cube
+        // gl.uniform4f(u_FragColor, rgba[0]*.9, rgba[1]*.9, rgba[2]*.9, rgba[3]);
+        // drawTriangle3D(gl, [0.0,1.0,0.0,  1.0,1.0,1.0,  1.0,1.0,0.0]);
+        // drawTriangle3D(gl, [0.0,1.0,0.0,  0.0,1.0,1.0,  1.0,1.0,1.0]);
 
-        // Bottom of Cube
-        drawTriangle3D(gl, [0.0,0.0,0.0,  1.0,0.0,1.0,  1.0,0.0,0.0]);
-        drawTriangle3D(gl, [0.0,0.0,0.0,  0.0,0.0,1.0,  1.0,0.0,1.0]);
+        // // Bottom of Cube
+        // gl.uniform4f(u_FragColor, rgba[0]*.7, rgba[1]*.7, rgba[2]*.7, rgba[3]);
+        // drawTriangle3D(gl, [0.0,0.0,0.0,  1.0,0.0,1.0,  1.0,0.0,0.0]);
+        // drawTriangle3D(gl, [0.0,0.0,0.0,  0.0,0.0,1.0,  1.0,0.0,1.0]);
 
+
+        gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+        //gl.uniform4f(u_FragColor, 1, 0.5, 1, 1);
+        drawTriangle3D(gl, [0.0, 0.0, 0.0,   1.0, 1.0, 0.0,   0.0, 1.0, 0.0]);
+        drawTriangle3D(gl, [0.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 1.0, 0.0]);
+        // xy1
+        gl.uniform4f(u_FragColor, rgba[0]*0.9, rgba[1]*0.9, rgba[2]*0.9, rgba[3]);
+        //gl.uniform4f(u_FragColor, 1, 1, 0, 1);
+        drawTriangle3D(gl, [1.0, 1.0, 1.0,   0.0, 0.0, 1.0,   0.0, 1.0, 1.0]);
+        drawTriangle3D(gl, [1.0, 0.0, 1.0,   0.0, 0.0, 1.0,   1.0, 1.0, 1.0]);
+        // 0yz
+        gl.uniform4f(u_FragColor, rgba[0]*0.8, rgba[1]*0.8, rgba[2]*0.8, rgba[3]);
+        //gl.uniform4f(u_FragColor, 1, 1, 1, 1);
+        drawTriangle3D(gl, [0.0, 1.0, 1.0,   0.0, 0.0, 0.0,   0.0, 1.0, 0.0]);
+        drawTriangle3D(gl, [0.0, 0.0, 1.0,   0.0, 0.0, 0.0,   0.0, 1.0, 1.0]);
+        // 1yz
+        gl.uniform4f(u_FragColor, rgba[0]*0.7, rgba[1]*0.7, rgba[2]*0.7, rgba[3]);
+        //gl.uniform4f(u_FragColor, 1, 0, 0, 1);
+        drawTriangle3D(gl, [1.0, 0.0, 0.0,   1.0, 1.0, 1.0,   1.0, 1.0, 0.0]);
+        drawTriangle3D(gl, [1.0, 0.0, 0.0,   1.0, 0.0, 1.0,   1.0, 1.0, 1.0]);
+        // x0z
+        gl.uniform4f(u_FragColor, rgba[0]*0.6, rgba[1]*0.6, rgba[2]*0.6, rgba[3]);
+        //gl.uniform4f(u_FragColor, 1, 0, 1, 1);
+        drawTriangle3D(gl, [1.0, 0.0, 0.0,   0.0, 0.0, 0.0,   1.0, 0.0, 1.0]);
+        drawTriangle3D(gl, [0.0, 0.0, 0.0,   0.0, 0.0, 1.0,   1.0, 0.0, 1.0]);
+        // x1z face
+        gl.uniform4f(u_FragColor, rgba[0]*0.7, rgba[1]*0.7, rgba[2]*0.7, rgba[3]);
+        //gl.uniform4f(u_FragColor, 0, 0, 1, 1);
+        drawTriangle3D(gl, [0.0, 1.0, 1.0,   0.0, 1.0, 0.0,   1.0, 1.0, 1.0]);
+        drawTriangle3D(gl,[1.0, 1.0, 1.0,   0.0, 1.0, 0.0,   1.0, 1.0, 0.0]);
+ 
     }
 }
--- asg2/asg2.js
+++ asg2/asg2.js
@@ -3,10 +3,9 @@
 var VSHADER_SOURCE =
   `attribute vec4 a_Position;
   uniform mat4  u_ModelMatrix;
-  uniform float u_Size;
+  uniform mat4 u_GlobalRotationMatrix;
   void main() {
-    gl_Position = u_ModelMatrix * a_Position;
-    gl_PointSize = u_Size;
+    gl_Position = u_GlobalRotationMatrix * u_ModelMatrix * a_Position;
   }`
 
 // Fragment shader program
@@ -21,8 +20,8 @@ let canvas;
 let gl;
 let a_Position;
 let u_FragColor;
-let u_Size;
 let u_ModelMatrix
+let u_GlobalRotationMatrix;
 function setupWebGL() {
   // Retrieve <canvas> element
   canvas = document.getElementById('webgl');
@@ -34,6 +33,7 @@ function setupWebGL() {
     console.log('Failed to get the rendering context for WebGL');
     return;
   }
+  // gl.enable(gl.DEPTH_TEST); // Enable depth test
 }
 
 function connectVariablesToGLSL() {
@@ -57,44 +57,37 @@ function connectVariablesToGLSL() {
     return;
   }
 
-  // Get the storage location of u_Size
-  u_Size = gl.getUniformLocation(gl.program, 'u_Size');
-  if (!u_Size) {
-    console.log('Failed to get the storage location of u_Size');
-    return;
-  }
-
   // Get the storage location of u_ModelMatrix
   u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
   if (!u_ModelMatrix) {
     console.log('Failed to get the storage location of u_ModelMatrix');
     return;
   }
+
+  u_GlobalRotationMatrix = gl.getUniformLocation(gl.program, 'u_GlobalRotationMatrix');
+  if (!u_GlobalRotationMatrix) {
+    console.log('Failed to get the storage location of u_GlobalRotationMatrix');
+    return;
+  }
 }
-const POINT = 0;
-const TRIANGLE = 1;
-const CIRCLE = 2;
-let g_selectedColor = [1.0, 1.0, 1.0, 1.0]; 
-let g_selectedSize = 10; 
-let g_selectedType=POINT;
-let g_segments = 10;
+// const POINT = 0;
+// const TRIANGLE = 1;
+// const CIRCLE = 2;
+// let g_selectedColor = [1.0, 1.0, 1.0, 1.0]; 
+// let g_selectedType=POINT;
+// let g_segments = 10;
+let g_globalAngle = 0.0;
+let g_jointSlider = 0.0;
+let g_purpleJoint = 0.0;
+let g_animation = true;
 function htmlUI() {
-  document.getElementById('green').onclick = function() { g_selectedColor = [0.0,1.0,0.0,1.0]; };
-  document.getElementById('red').onclick = function() { g_selectedColor = [1.0,0.0,0.0,1.0]; };
-  document.getElementById('clear').onclick = function() { g_shapes = []; renderShapes(); };
-  document.getElementById('drawing').onclick = function() { drawPicture(); };
+  document.getElementById('on').onclick = function() { g_animation = true; };
+  document.getElementById('off').onclick = function() { g_animation = false; };
 
-  document.getElementById('point').onclick = function() { g_selectedType =POINT};
-  document.getElementById('triangle').onclick = function() { g_selectedType =TRIANGLE };
-  document.getElementById('circle').onclick = function() { g_selectedType =CIRCLE };
-  document.getElementById('etch').onclick = function() { etchASketch(); };
-
-  //sliders
-  document.getElementById('redslide').addEventListener('mouseup', function() { g_selectedColor[0] = this.value/100; });
-  document.getElementById('greenslide').addEventListener('mouseup', function() { g_selectedColor[1] = this.value/100; });
-  document.getElementById('blueslide').addEventListener('mouseup', function() { g_selectedColor[2] = this.value/100; });
-  document.getElementById('sizeslide').addEventListener('mouseup', function() { g_selectedSize = this.value; });
-  document.getElementById('segmentslide').addEventListener('mouseup', function() { g_segments = this.value; });
+  // //sliders
+  document.getElementById('angleslide').addEventListener('mousemove', function() { g_globalAngle = this.value; renderShapes(); });
+  document.getElementById('jointslide').addEventListener('mousemove', function() { g_jointSlider = this.value; renderShapes(); });
+  document.getElementById('purplejointslide').addEventListener('mousemove', function() { g_purpleJoint = this.value; renderShapes(); });
 }
 function main() {
 
@@ -110,7 +103,8 @@ function main() {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
   // Clear <canvas>
-  renderShapes();
+  // renderShapes();
+  requestAnimationFrame(tick);
 }
 
 var g_shapes = []; 
@@ -125,78 +119,94 @@ function coordsToGL(ev) {
   return [x, y];
 } 
 
+g_startTime = performance.now() / 1000.0;
+g_seconds = performance.now() / 1000.0 - g_startTime;
 
+function tick() {
+  g_seconds = performance.now() / 1000.0 - g_startTime;
+  console.log(g_seconds);
+
+  updateAnimationAngle();
+
+  renderShapes();
+  requestAnimationFrame(tick);
+}
+
+function updateAnimationAngle() {
+  if (g_animation) {
+    g_jointSlider = 45*Math.sin(g_seconds);
+    g_purpleJoint = 45*Math.cos(g_seconds);
+  }
+}
 
 function renderShapes() { 
   // Clear <canvas>
   // var startTime = performance.now();
-  gl.clear(gl.COLOR_BUFFER_BIT);
 
+  var globalRotMat = new Matrix4().rotate(g_globalAngle, 0, 1, 0);
+  gl.uniformMatrix4fv(u_GlobalRotationMatrix, false, globalRotMat.elements);
 
-  // Test triangle
-  gl.uniform4f(u_FragColor, 0.9, 0.9, 0.4, 1.0); 
-  const modelMatrix = new Matrix4(); 
-  gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
-  drawTriangle3D(gl, [-1.0,0.0,0.0,  -0.5,-1.0,0.0,  0.0,0.0,0.0]);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); 
+
+  gl.enable(gl.CULL_FACE);
+  gl.cullFace(gl.BACK);
+  gl.enable(gl.DEPTH_TEST);
+
+  // // Test triangle
+  // gl.uniform4f(u_FragColor, 0.9, 0.9, 0.4, 1.0); 
+  // const modelMatrix = new Matrix4(); 
+  // gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
+  // drawTriangle3D(gl, [-1.0,0.0,0.0,  -0.5,-1.0,0.0,  0.0,0.0,0.0]);
 
   // Draw Cube
   var body = new Cube();
   body.color = [1.0, 0.0, 0.0, 1.0];
-  body.matrix.translate(-.25,-.5,0.0);
-  body.matrix.scale(0.5,1,0.5);
+  body.matrix.translate(-.25,-.75,0.0);
+  body.matrix.rotate(-5,1,0,0);
+  body.matrix.scale(0.5,.3,0.5);
   body.render()
 
   // Draw Cube
-  var body = new Cube();
-  body.color = [1.0, 1.0, 0.0, 1.0];
-  body.matrix.translate(.7,0,0.0);
-  body.matrix.rotate(45,0,0,1);
-  body.matrix.scale(0.25,.7,0.5);
-  body.render()
+  var arm = new Cube();
+  arm.color = [1.0, 1.0, 0.0, 1.0];
+  arm.matrix.translate(0,-.5,0.0);
+  arm.matrix.rotate(-5,1,0,0);
+  arm.matrix.rotate(g_jointSlider,0,0,1);
+  var yellowCoordinates = new Matrix4(arm.matrix);
+  arm.matrix.scale(0.25,.7,0.5);
+  arm.matrix.translate(-0.5,0,0);
+  arm.render()
+
+  var box = new Cube();
+  box.color = [1,0,1,1];
+  box.matrix = yellowCoordinates
+  box.matrix.translate(0,.65,0)
+  // box.matrix.rotate(g_purpleJoint,0,0,1)
+  box.matrix.rotate(g_purpleJoint,0,0,1)
+  box.matrix.scale(.3,.3,.3)
+  box.matrix.translate(-.5,0,-0.0001)
+  // box.matrix.translate(-.1,.1,0.0);
+  // box.matrix.rotate(-30,1,0,0);
+  // box.matrix.scale(0.2,.4,.2);
+  box.render()
+
+  var k = 10;
+  for (var i = 1; i < k; i++) {
+    var c = new Cube();
+    c.matrix.translate(-.8,1.9*i/k-1,0.0);
+    c.matrix.rotate(g_seconds*1000,1,1,1);
+    c.matrix.scale(.1,.5/k,.1/k)
+    c.color = [
+      (Math.sin(g_seconds * i) * 0.5 + 0.5), 
+      (Math.cos(g_seconds * i * 1.5) * 0.5 + 0.5), 
+      (Math.sin(g_seconds * i * 2) * 0.5 + 0.5), 
+      1
+    ];
+    c.render()
+  }
 
   // var duration = performance.now() - startTime;
   // sendTextToHTML("perf", "numdot " + len + " ms " + duration + " fps " + 1000/duration);
-}
-
-function click(ev) {
-  etchMode = false; 
-  document.removeEventListener('keydown', handleEtchKeyPress);
-  sendTextToHTML("isetch", "");
-  // Get the coordinates of a mouse press
-  [x,y] = coordsToGL(ev);
-  // Store the coordinates to g_points array
-  // g_points.push([x, y]);
-
-  // g_colors.push(g_selectedColor.slice());
-
-  // g_sizes.push(g_selectedSize);
-
-  // let point = new Triangle();
-  let point;
-  if (g_selectedType == POINT) {
-    point = new Point();
-  } else if (g_selectedType == TRIANGLE) {
-    point = new Triangle();
-  } else if (g_selectedType == CIRCLE) {
-    point = new Circle();
-    point.segments = g_segments;
-  }
-  point.position = [x,y];
-  
-  point.color = g_selectedColor.slice();
-  point.size = g_selectedSize;
-  g_shapes.push(point);
-  // Store the coordinates to g_points array
-  // if (x >= 0.0 && y >= 0.0) {      // First quadrant
-  //   g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
-  // } else if (x < 0.0 && y < 0.0) { // Third quadrant
-  //   g_colors.push([0.0, 1.0, 0.0, 1.0]);  // Green
-  // } else {                         // Others
-  //   g_colors.push([1.0, 1.0, 1.0, 1.0]);  // White
-  // }
-  // Render the shapes
-  renderShapes(ev);
-  
 }
 
 function sendTextToHTML(id, text) {
