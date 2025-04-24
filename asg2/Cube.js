33885f329a59b0817005167ce44af840333b0cc7
Ariel Raizman <ariel@appesteem.com>
2025-04-24 16:21:48 -0700

    remove red debug face

--- asg2/Cube.js
+++ asg2/Cube.js
@@ -18,7 +18,7 @@ class Cube {
         gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
 
         // Front of Cube
-        gl.uniform4f(u_FragColor, 255, 0, 0, rgba[3]);
+        // gl.uniform4f(u_FragColor, 255, 0, 0, rgba[3]);
         drawTriangle3D(gl, [0.0, 0.0, 0.0,   1.0, 1.0, 0.0,   0.0, 1.0, 0.0]);
         drawTriangle3D(gl, [0.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 1.0, 0.0]);
 
@@ -50,31 +50,6 @@ class Cube {
         gl.uniform4f(u_FragColor, rgba[0]*.7, rgba[1]*.7, rgba[2]*.7, rgba[3]);
         drawTriangle3D(gl, [0.0, 1.0, 1.0,   0.0, 1.0, 0.0,   1.0, 1.0, 1.0]);
         drawTriangle3D(gl,[1.0, 1.0, 1.0,   0.0, 1.0, 0.0,   1.0, 1.0, 0.0]);
-
-
-        gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-        //gl.uniform4f(u_FragColor, 1, 0.5, 1, 1);
-        
-        // xy1
-        
-        //gl.uniform4f(u_FragColor, 1, 1, 0, 1);
-        
-        // 0yz
-        
-        //gl.uniform4f(u_FragColor, 1, 1, 1, 1);
-        
-        // 1yz
-        
-        //gl.uniform4f(u_FragColor, 1, 0, 0, 1);
-        
-        // x0z
-        
-        //gl.uniform4f(u_FragColor, 1, 0, 1, 1);
-        
-        // x1z face
-        gl.uniform4f(u_FragColor, rgba[0]*0.7, rgba[1]*0.7, rgba[2]*0.7, rgba[3]);
-        //gl.uniform4f(u_FragColor, 0, 0, 1, 1);
-        
  
     }
 }
